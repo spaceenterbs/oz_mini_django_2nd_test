4767f87dd38f112d76ee7697c3e619b88b4164bb
enterspacebs <anotherhell48@gmail.com>
2023-07-17 03:13:20 +0900

    is it last?

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 poetry install --no-root
+poetry add 'whitenoise[brotli]'
 
 python manage.py collectstatic --no-input # 모든 static 파일을 수집해, css, js파일을 모두 모아서 같은 폴더에 넣어준다.
 python manage.py migrate
